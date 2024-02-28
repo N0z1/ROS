97cf8b03a2589d583bf1e337b67ce6bc7d4fa780
Nassor  Y Salim <142937035+N0z1@users.noreply.github.com>
2024-02-28 13:16:52 +0000

    Add files via upload

---
+++ OpenCVtests/lab1_3/CMakeFiles/ObjectDetect.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+FILE(REMOVE_RECURSE
+  "CMakeFiles/ObjectDetect.dir/ObjectDetect.cpp.o"
+  "CMakeFiles/ObjectDetect.dir/my_blobs.cpp.o"
+  "ObjectDetect.pdb"
+  "ObjectDetect"
+)
+
+# Per-language clean rules from dependency scanning.
+FOREACH(lang CXX)
+  INCLUDE(CMakeFiles/ObjectDetect.dir/cmake_clean_${lang}.cmake OPTIONAL)
+ENDFOREACH(lang)
